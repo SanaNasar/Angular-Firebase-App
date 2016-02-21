8fd4e6c4b1a3bbb8e86bdbc92b6e43693d008a82
SanaNasar <sanasehereen.cse@gmail.com>
2016-02-21 09:46:50 -0800

    making changes to app.js

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+// Define the routes
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+     // Routes will be here
 }]);
