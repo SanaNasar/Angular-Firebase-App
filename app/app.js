203cb8cd2b334f22c6eca042690ab71b60f1a0d0
SanaNasar <sanasehereen.cse@gmail.com>
2016-02-21 09:51:30 -0800

    making changes to app.js

--- app/app.js
+++ app/app.js
@@ -3,8 +3,13 @@
 // Declare app level module which depends on views, and components
 // Define the routes
 angular.module('myApp', [
-  'ngRoute'
+  'ngRoute',
+  'myApp.home'           // Newly added home module
 ]).
 config(['$routeProvider', function($routeProvider) {
      // Routes will be here
+     // Set default view of our app as home
+      $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
 }]);
